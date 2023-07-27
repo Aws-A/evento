a4de7e093392e13d3e10ef95a470b33f6de4497f
William <Williamtviet@hotmail.com>
2023-07-27 13:27:17 -0700

    removed a lot of default unneeded code

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+
 import './App.css';
 import LoginButton from './components/LoginButton';
 import LogoutButton from './components/LogoutButton';
@@ -6,22 +6,8 @@ import LogoutButton from './components/LogoutButton';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <LoginButton/>
         <LogoutButton/>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
