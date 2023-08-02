eadbc3931f1cc5ed9b7a462ca4b811bad978a5aa
William <Williamtviet@hotmail.com>
2023-08-02 12:53:02 -0700

    adjust profile page

--- src/components/ProfilePage.js
+++ src/components/ProfilePage.js
@@ -1,39 +1,44 @@
+// ProfilePage.js
 import React from "react";
-import { useAuth0 } from "@auth0/auth0-react"
+import { useAuth0 } from "@auth0/auth0-react";
+import "./ProfilePage.css";
 
 const ProfilePage = () => {
-  const { user, isAuthenticated } = useAuth0()
+  const { user, isAuthenticated } = useAuth0();
   if (isAuthenticated && user) {
     const { name, email } = user;
-    console.log (user.data)
+    console.log(user.data);
 
     return (
       <>
         <h1>Profile</h1>
         {user.picture && (
-              <img
-                src={user.picture}
-                alt="Profile Picture"
-                style={{ width: "100px", height: "100px", borderRadius: "50%" }}
-              />
-            )}
-        <h1>Upload Profile Picture</h1>
+          <img
+            src={user.picture}
+            alt="Profile Picture"
+            style={{ width: "100px", height: "100px", borderRadius: "50%" }}
+          />
+        )}
+        <button>Upload Profile Picture</button>
         <div className="ProfileEdits">
-          <h1>Email</h1>
-          <textarea placeholder="Enter your email here" />
-          <p>{user.email}</p>
-
-
-          <h1>First Name</h1>
-          <h2>{user?.given_name}</h2>
-
-          <h1>Last Name</h1>
-          <h2>{user?.family_name}</h2>
-
-        
-          <h1>Phone Number</h1>
-          <textarea placeholder="Enter your phone number here" />
+          <div>
+            <h1>First Name</h1>
+            <h2>{user?.given_name}</h2>
+          </div>
+          <div>
+            <h1>Last Name</h1>
+            <h2>{user?.family_name}</h2>
+          </div>
+          <div>
+            <h1>Phone Number</h1>
+            <textarea placeholder="Enter your phone number here" />
+          </div>
+          <div>
+            <h1>Email</h1>
+            <textarea placeholder="Enter your email here" value={user.email} readOnly />
+          </div>
         </div>
+        <button>Save Changes</button>
       </>
     );
   } else {
