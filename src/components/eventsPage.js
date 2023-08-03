ad24e2aebcf8ef413a09838b3157dfa0c161347d
William <Williamtviet@hotmail.com>
2023-08-03 08:50:39 -0700

    added the 3 search option in the homepage, but they dont query just yet

--- src/components/eventsPage.js
+++ src/components/eventsPage.js
@@ -1,10 +1,76 @@
-const EventsPage = () => {
+import React, { useState } from "react";
+
+const EventsPage= () => {
+  const [eventSearchQuery, setEventSearchQuery] = useState("");
+  const [eventSearchResults, setEventSearchResults] = useState([]);
+
+  const [groupSearchQuery, setGroupSearchQuery] = useState("");
+  const [groupSearchResults, setGroupSearchResults] = useState([]);
+
+  const [interestSearchQuery, setInterestSearchQuery] = useState("");
+  const [interestSearchResults, setInterestSearchResults] = useState([]);
 
   return (
-    <> 
-<h1> the explore/events page</h1>
-    </>
-   
-  )
-}
-export default EventsPage
+    <>
+    <div className="eventSearch">
+      <h1>Search</h1>
+      <div>
+        {/* <h2>Events</h2> */}
+        <input
+          type="text"
+          value={eventSearchQuery}
+          onChange={(e) => setEventSearchQuery(e.target.value)}
+          placeholder="Search events..."
+        />
+        {/* Display event search results */}
+        <ul>
+          {eventSearchResults.map((result) => (
+            <li key={result.id}>{result.name}</li>
+          ))}
+        </ul>
+      </div>
+      <div>
+        {/* <h2>Groups</h2> */}
+        <input
+          type="text"
+          value={groupSearchQuery}
+          onChange={(e) => setGroupSearchQuery(e.target.value)}
+          placeholder="Search groups..."
+        />
+        {/* Display group search results */}
+        <ul>
+          {groupSearchResults.map((result) => (
+            <li key={result.id}>{result.name}</li>
+          ))}
+        </ul>
+      </div>
+      <div>
+        {/* <h2>Interests</h2> */}
+        <input
+          type="text"
+          value={interestSearchQuery}
+          onChange={(e) => setInterestSearchQuery(e.target.value)}
+          placeholder="Search interests..."
+        />
+        {/* Display interest search results */}
+        <ul>
+          {interestSearchResults.map((result) => (
+            <li key={result.id}>{result.name}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
+     <ul>
+     Events (somehow use li key for each event thought this involves using db)
+   </ul>
+   </>
+  
+
+
+
+
+
+  );
+};
+
+export default EventsPage;
