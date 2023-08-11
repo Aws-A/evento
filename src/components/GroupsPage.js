4a4735c57c4a8ea34e3b6e69c73c73064c57a335
William <Williamtviet@hotmail.com>
2023-08-11 15:18:51 -0700

    added the groups page that renders all groups as a list with search functions

--- src/components/GroupsPage.js
+++ src/components/GroupsPage.js
@@ -1,18 +1,70 @@
-import React, { useState } from "react";
+
 import "./groups.css"
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+import './EventsPage.css'
+
 
 const GroupsPage = (props) => {
+  const [groupSearchQuery, setGroupSearchQuery] = useState("");
+  const [groupSearchResults, setGroupSearchResults] = useState([]);
+  const [groups, setGroups] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:8080/groups')
+      .then(response => {
+        setGroups(response.data);
+      })
+      .catch(error => {
+        console.error('Error fetching groups data:', error);
+      });
+  }, []);
+
+  useEffect(() => {
+    const filteredGroups = groups.filter(group =>
+      group.groupname.toLowerCase().includes(groupSearchQuery.toLowerCase()) ||
+      group.groupdescription.toLowerCase().includes(groupSearchQuery.toLowerCase())
+    );
+    setGroupSearchResults(filteredGroups);
+  }, [groupSearchQuery, groups]);
+
+  const handleGroupClick = (selectedGroup) => {
+    props.onHomeChange("chosenPage", selectedGroup);
+  };
 
   return (
-    <> 
-      <h1 class="title"> Groups</h1>
-      <div class="eventB">
-        <div> </div>
-        <div> </div>
+    <>
+      <div className="eventSearch">
+    
+        <h1>Search</h1>
+        <div>
+          <input
+            type="text"
+            value={groupSearchQuery}
+            onChange={(e) => setGroupSearchQuery(e.target.value)}
+            placeholder="Search groups..."
+          />
+        </div>
+        <div></div>
       </div>
-      <div onClick={() => props.onHomeChange("chosenPageGr")}> Aws Temporary Link </div>
+
+      <ul>
+        {groupSearchResults.map((selectedGroup, index) => (
+          <li
+            key={index}
+            className="event-box"
+            onClick={() => handleGroupClick(selectedGroup)}
+          >
+            <strong>{selectedGroup.groupname}</strong>
+            <p>{selectedGroup.groupdescription}</p>
+  
+          </li>
+        ))}
+      </ul>
+
+      <div onClick={() => props.onHomeChange("chosenPage")}>Aws Temporary Link</div>
     </>
-   
-  )
-}
-export default GroupsPage
+  );
+};
+
+export default GroupsPage;
