81477a7abe0b80fc4d349fb3ae02f330bc7f2148
William <Williamtviet@hotmail.com>
2023-08-12 17:26:51 -0700

    now on login it adds user tp the db if user name doesnt exist, BUT cant seem to get it to stop even if it already exists right now

--- src/components/LoginButton.js
+++ src/components/LoginButton.js
@@ -1,32 +1,43 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 import axios from "axios";
 
-//also deals with login functionality
 const LoginButton = () => {
   const { loginWithRedirect, user, isAuthenticated } = useAuth0();
+  const [nameExists, setNameExists] = useState(false); // New state to track name existence
 
   useEffect(() => {
-    if (isAuthenticated && user) {
-      const { name, email, nickname } = user;
+    const checkNameExists = async () => {
+      if (isAuthenticated && user) {
+        const { given_name, family_name } = user;
 
-      axios.get(`http://localhost:8080/user/names`)
-        .then(response => {
-          const names =response.data
-          console.log("Backend response:", names);
+        try {
+          const response = await axios.get(`http://localhost:8080/user/names`);
+          const names = response.data;
 
-          if (names.includes(name)) {
-            // User's name already exists in the database
-            console.log("User's name already exists");
+          const exists = names.some(n => n === `${given_name} ${family_name}`);
+          setNameExists(exists); // Update the state with the result
+
+          if (!exists) {
+            console.log("User's name does not exist, creating new user");
+            const placeholderEmail = `${given_name}.${family_name}@hotmail.com`;
+            const addUserResponse = await axios.post(`http://localhost:8080/user/users`, {
+              firstName: given_name,
+              lastName: family_name,
+              email: placeholderEmail,
+              password: "password"
+            });
+            console.log("User added:", addUserResponse.data);
           } else {
-            // User's name does not exist in the database
-            console.log("User's name does not exist");
+            console.log("User's name already exists and is in the DB");
           }
-        })
-        .catch(error => {
-          console.error("Error checking user's name:", error);
-        });
+        } catch (error) {
+          console.error("Error:", error);
+        }
       }
+    };
+
+    checkNameExists();
   }, [isAuthenticated, user]);
 
   const handleLogin = () => {
@@ -38,9 +49,12 @@ const LoginButton = () => {
   };
 
   return (
-    <button onClick={handleLogin} className="log">
-      Log In
-    </button>
+    
+      <button onClick={handleLogin} className="log">
+        Log In
+      </button>
+     
+    
   );
 };
 
